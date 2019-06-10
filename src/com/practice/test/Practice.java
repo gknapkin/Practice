e01ba7434a20492230bd18b7ca7c99e932132027
Gary Knapik <garyknapik@Garys-MacBook-Pro.local>
2019-06-09 23:21:53 -0400

    Added a line of code string

--- src/com/practice/test/Practice.java
+++ src/com/practice/test/Practice.java
@@ -4,6 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("This is a test for github");
 
-
+        System.out.println("I've made a change #1");
     }
 }
